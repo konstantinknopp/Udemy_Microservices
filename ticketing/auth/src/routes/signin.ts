a6a8beee1785884ac12ac37c339b5482c5de1b11
Konstantin Knopp <k.knopp@das-unikat.com>
2022-10-07 15:59:08 +0200

    ticketing

---
+++ ticketing/auth/src/routes/signin.ts
@@ -0,0 +1,9 @@
+import * as express from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/signin', (req, res) => {
+    res.send('Hi there!');
+});
+
+export {router as signinRouter};
