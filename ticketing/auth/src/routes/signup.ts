e08c10f006f1d0a1ea03148ea68849f8d33cfbf4
Konstantin Knopp <k.knopp@das-unikat.com>
2022-10-11 05:19:49 +0200

    .

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator'; 
+import jwt from 'jsonwebtoken';
+
 import { BadRequestError } from '../errors/bad-request-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 
@@ -32,6 +33,15 @@ router.post('/api/users/signup', [
 
         const user = User.build({email,password});
         await user.save();
+
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!);
+
+        req.session = {
+            jwt: userJwt
+        }
 
         res.status(201).send(user);
     }
