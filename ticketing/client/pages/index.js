3598d16f1d6079cc546d157ce6870744277dfbe7
Konstantin Knopp <k.knopp@das-unikat.com>
2022-10-11 20:20:26 +0200

    .

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,5 +1,20 @@
-const Index = () => {
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
+    console.log(currentUser);
+    axios.get('/api/users/currentuser').catch(err => {
+        console.log(err.message);
+    });
+
     return <h1>Landing Page</h1>
 };
 
-export default Index;
+
+LandingPage.getInitialProps = async () => {
+    const response = await axios.get('/api/users/currentuser');
+
+    return response.data;
+};
+
+
+export default LandingPage;
