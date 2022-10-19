1b441610b7228fc931323385d71819e790ab98c2
Konstantin Knopp <k.knopp@das-unikat.com>
2022-10-19 15:14:08 +0200

    Updates

--- ticketing/common/src/events/types/order-status.ts
+++ ticketing/common/src/events/types/order-status.ts
@@ -1,8 +1,6 @@
-enum OrderStatus {
+export enum OrderStatus {
     Created = 'created',
     Cancelled = 'cancelled',
     AwaitingPayment = 'awaiting:payment',
     Complete = 'complete'
 }
-
-export default OrderStatus;
